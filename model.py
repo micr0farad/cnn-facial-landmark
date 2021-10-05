21d86ed28dc9ac942cf5dee1f3a44400fb03e65e
Mikhail Gribkov <faust_32@inbox.ru>
2021-10-05 11:23:19 +0300

    pytorch train

--- model.py
+++ model.py
@@ -1,143 +1,117 @@
 """This module provides the function to build the network."""
-import tensorflow as tf
-from tensorflow import keras
+import torch
+from torch.nn import Sequential
 
 
-def build_landmark_model(input_shape, output_size):
+def build_landmark_model(output_size) -> Sequential:
     """Build the convolutional network model with Keras Functional API.
 
     Args:
-        input_shape: the shape of the input image, without batch size.
-        output_size: the number of output node, usually equals to the number of
-            marks times 2 (in 2d space).
+        output_size: the number of output node, usually equals to the number of marks times 2 (in 2d space).
 
     Returns:
-        a Keras model, not compiled.
+        a model
     """
-
-    # The model is composed of multiple layers.
-
-    # Preprocessing layers.
-    preprocess = keras.layers.experimental.preprocessing.Normalization()
-
-    # Convolutional layers.
-    conv_1 = keras.layers.Conv2D(filters=32,
-                                 kernel_size=(3, 3),
-                                 activation='relu')
-    conv_2 = keras.layers.Conv2D(filters=64,
-                                 kernel_size=(3, 3),
-                                 strides=(1, 1),
-                                 padding='valid',
-                                 activation='relu')
-    conv_3 = keras.layers.Conv2D(filters=64,
-                                 kernel_size=(3, 3),
-                                 strides=(1, 1),
-                                 padding='valid',
-                                 activation='relu')
-    conv_4 = keras.layers.Conv2D(filters=64,
-                                 kernel_size=(3, 3),
-                                 strides=(1, 1),
-                                 padding='valid',
-                                 activation='relu')
-    conv_5 = keras.layers.Conv2D(filters=64,
-                                 kernel_size=[3, 3],
-                                 strides=(1, 1),
-                                 padding='valid',
-                                 activation='relu')
-    conv_6 = keras.layers.Conv2D(filters=128,
-                                 kernel_size=(3, 3),
-                                 strides=(1, 1),
-                                 padding='valid',
-                                 activation='relu')
-    conv_7 = keras.layers.Conv2D(filters=128,
-                                 kernel_size=[3, 3],
-                                 strides=(1, 1),
-                                 padding='valid',
-                                 activation='relu')
-    conv_8 = keras.layers.Conv2D(filters=256,
-                                 kernel_size=[3, 3],
-                                 strides=(1, 1),
-                                 padding='valid',
-                                 activation='relu')
-
-    # Pooling layers.
-    pool_1 = keras.layers.MaxPool2D(pool_size=(2, 2),
-                                    strides=(2, 2),
-                                    padding='valid')
-    pool_2 = keras.layers.MaxPool2D(pool_size=(2, 2),
-                                    strides=(2, 2),
-                                    padding='valid')
-    pool_3 = keras.layers.MaxPool2D(pool_size=(2, 2),
-                                    strides=(2, 2),
-                                    padding='valid')
-    pool_4 = keras.layers.MaxPool2D(pool_size=[2, 2],
-                                    strides=(1, 1),
-                                    padding='valid')
-
-    # Dense layers.
-    dense_1 = keras.layers.Dense(units=1024,
-                                 activation='relu',
-                                 use_bias=True)
-    dense_2 = keras.layers.Dense(units=output_size,
-                                 activation=None,
-                                 use_bias=True)
-    
-    # Batch norm layers
-    bn_1 = keras.layers.BatchNormalization()
-    bn_2 = keras.layers.BatchNormalization()
-    bn_3 = keras.layers.BatchNormalization()
-    bn_4 = keras.layers.BatchNormalization()
-    bn_5 = keras.layers.BatchNormalization()
-    bn_6 = keras.layers.BatchNormalization()
-    bn_7 = keras.layers.BatchNormalization()
-    bn_8 = keras.layers.BatchNormalization()
-    bn_9 = keras.layers.BatchNormalization()
-
-
-    # Flatten layers.
-    flatten_1 = keras.layers.Flatten()
-
-    # All layers got. Define the forward propgation.
-    inputs = keras.Input(shape=input_shape, name="image_input")
-
-    # Preprocess the inputs.
-    x = preprocess(inputs)
-
-    # |== Layer 1 ==|
-    x = conv_1(x)
-    x = bn_1(x)
-    x = pool_1(x)
-
-    # |== Layer 2 ==|
-    x = conv_2(x)
-    x = bn_2(x)
-    x = conv_3(x)
-    x = bn_3(x)
-    x = pool_2(x)
-
-    # |== Layer 3 ==|
-    x = conv_4(x)
-    x = bn_4(x)
-    x = conv_5(x)
-    x = bn_5(x)
-    x = pool_3(x)
-
-    # |== Layer 4 ==|
-    x = conv_6(x)
-    x = bn_6(x)
-    x = conv_7(x)
-    x = bn_7(x)
-    x = pool_4(x)
-
-    # |== Layer 5 ==|
-    x = conv_8(x)
-    x = bn_8(x)
-
-    # |== Layer 6 ==|
-    x = flatten_1(x)
-    x = dense_1(x)
-    x = bn_9(x)
-    outputs = dense_2(x)
-
-    # Return the model
-    return keras.Model(inputs=inputs, outputs=outputs, name="landmark")
+    return torch.nn.Sequential(
+        # |== Layer 1 ==|
+        torch.nn.Conv2d(
+            in_channels=3,
+            out_channels=32,
+            kernel_size=(3, 3),
+        ),
+        torch.nn.ReLU(),
+        torch.nn.LazyBatchNorm2d(),
+        torch.nn.MaxPool2d(
+            kernel_size=(2, 2),
+            stride=(2, 2),
+            padding=(0, 0)
+        ),
+        # |== Layer 2 ==|
+        torch.nn.Conv2d(
+            in_channels=32,
+            out_channels=64,
+            stride=(1, 1),
+            padding=(0, 0),
+            kernel_size=(3, 3),
+        ),
+        torch.nn.ReLU(),
+        torch.nn.LazyBatchNorm2d(),
+        torch.nn.Conv2d(
+            in_channels=64,
+            out_channels=64,
+            stride=(1, 1),
+            padding=(0, 0),
+            kernel_size=(3, 3),
+        ),
+        torch.nn.ReLU(),
+        torch.nn.LazyBatchNorm2d(),
+        torch.nn.MaxPool2d(
+            kernel_size=(2, 2),
+            stride=(2, 2),
+            padding=(0, 0)
+        ),
+        # |== Layer 3 ==|
+        torch.nn.Conv2d(
+            in_channels=64,
+            out_channels=64,
+            stride=(1, 1),
+            padding=(0, 0),
+            kernel_size=(3, 3),
+        ),
+        torch.nn.ReLU(),
+        torch.nn.LazyBatchNorm2d(),
+        torch.nn.Conv2d(
+            in_channels=64,
+            out_channels=64,
+            stride=(1, 1),
+            padding=(0, 0),
+            kernel_size=(3, 3),
+        ),
+        torch.nn.ReLU(),
+        torch.nn.LazyBatchNorm2d(),
+        torch.nn.MaxPool2d(
+            kernel_size=(2, 2),
+            stride=(2, 2),
+            padding=(0, 0)
+        ),
+        # |== Layer 4 ==|
+        torch.nn.Conv2d(
+            in_channels=64,
+            out_channels=128,
+            stride=(1, 1),
+            padding=(0, 0),
+            kernel_size=(3, 3),
+        ),
+        torch.nn.ReLU(),
+        torch.nn.LazyBatchNorm2d(),
+        torch.nn.Conv2d(
+            in_channels=128,
+            out_channels=128,
+            stride=(1, 1),
+            padding=(0, 0),
+            kernel_size=(3, 3),
+        ),
+        torch.nn.ReLU(),
+        torch.nn.LazyBatchNorm2d(),
+        torch.nn.MaxPool2d(
+            kernel_size=(2, 2),
+            stride=(1, 1),
+            padding=(0, 0)
+        ),
+        # |== Layer 5 ==|
+        torch.nn.Conv2d(
+            in_channels=128,
+            out_channels=256,
+            stride=(1, 1),
+            padding=(0, 0),
+            kernel_size=(3, 3),
+        ),
+        torch.nn.ReLU(),
+        torch.nn.LazyBatchNorm2d(),
+        # |== Layer 6 ==|
+        torch.nn.Flatten(),
+        torch.nn.LazyLinear(out_features=1024),
+        torch.nn.ReLU(),
+        torch.nn.LazyBatchNorm1d(),
+        torch.nn.LazyLinear(out_features=output_size)
+    )
