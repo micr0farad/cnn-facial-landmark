70cee62238d6dcf836e435a17110bf7ff74496b0
Mikhail Gribkov <67796080+micr0farad@users.noreply.github.com>
2021-10-06 17:56:13 +0300

    Feature/pytorch (#1)
    Model realigned to pytorch

--- landmark.py
+++ landmark.py
@@ -2,110 +2,119 @@
 Convolutional Neural Network for facial landmarks detection.
 """
 import argparse
+from typing import Iterator
 
-import numpy as np
-import tensorflow as tf
-from tensorflow import keras
+import cv2
+import matplotlib.pyplot as plt
+import numpy
+import torch
+from torch import optim
+from torch.utils.data import DataLoader
 
-from dataset import get_parsed_dataset
-from model import build_landmark_model
+from IterableTFRecordDataset import IterableTFRecordDataset
+from model import get_landmark_model
 
-# Introduce arguments parser to give user the flexibility to tune the process.
-parser = argparse.ArgumentParser()
-parser.add_argument('--train_record', default='train.record', type=str,
-                    help='Training record file')
-parser.add_argument('--val_record', default='validation.record', type=str,
-                    help='validation record file')
-parser.add_argument('--epochs', default=1, type=int,
-                    help='epochs for training')
-parser.add_argument('--batch_size', default=16, type=int,
-                    help='training batch size')
-parser.add_argument('--export_only', default=False, type=bool,
-                    help='Save the model without training and evaluation.')
-parser.add_argument('--eval_only', default=False, type=bool,
-                    help='Do evaluation without training.')
-args = parser.parse_args()
+IMAGE = "image/encoded"
+MARKS = "label/marks"
+INPUT_SHAPE = (128, 128, 3)
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--train_record', default='train.record', type=str, help='Training record file')
+    parser.add_argument('--train_index', default='train.index', type=str, help='Training record index file')
+    parser.add_argument('--epochs', default=1, type=int, help='epochs for training')
+    parser.add_argument('--batch_size', default=16, type=int, help='training batch size')
+    return parser.parse_args()
+
+
+def decode_image(features: dict):
+    features[IMAGE] = numpy.reshape(cv2.imdecode(features[IMAGE], -1), INPUT_SHAPE[::-1])
+    return features
+
+
+def get_data_loader(dataset: torch.utils.data.Dataset, batch_size: int) -> DataLoader:
+    return DataLoader(dataset, batch_size=batch_size)
+
+
+def get_loss_function() -> torch.nn.Module:
+    return torch.nn.MSELoss()
+
+
+def get_optimizer(parameters: Iterator[torch.nn.Parameter], lr: float) -> torch.optim.Optimizer:
+    return optim.Adam(params=parameters, lr=lr)
+
+
+def split_dataset(dataset, validation_size):
+    return torch.utils.data.random_split(dataset, (len(dataset) - validation_size, validation_size))
+
+
+def main(train_record: str, train_index: str, batch_size: int, lr: float, output_size: int, validation_size: int):
+    train_set, val_set = split_dataset(
+        IterableTFRecordDataset(
+            train_record,
+            train_index,
+            description={IMAGE: "byte", MARKS: "byte"},
+            shuffle_queue_size=1024,
+            transform=decode_image
+        ),
+        validation_size
+    )
+    model: torch.nn.Module = get_landmark_model(output_size=output_size)
+    criterion: torch.nn.Module = get_loss_function()
+    optimizer: torch.optim.Optimizer = get_optimizer(model.parameters(), lr)
+    train_data_loader = get_data_loader(train_set, batch_size)
+    val_data_loader = get_data_loader(val_set, batch_size)
+    train_losses = []
+    val_losses = []
+    for epoch in range(args.epochs):
+        train_loss = []
+        val_loss = []
+
+        model.train()
+        for data in iter(train_data_loader):
+            optimizer.zero_grad()
+            outputs = model.forward(data[IMAGE].float().cuda())
+            targets = data[MARKS].float().cuda()
+            loss = criterion.forward(outputs, targets)
+            loss.backward()
+            optimizer.step()
+            train_loss.append(loss.item())
+
+        with torch.no_grad():
+            model.eval()
+            for data in iter(val_data_loader):
+                outputs = model.forward(data[IMAGE].float().cuda())
+                targets = data[MARKS].float().cuda()
+                loss = criterion.forward(outputs, targets)
+                val_loss.append(loss.item())
+
+        avg_train_loss = sum(train_loss) / len(train_loss)
+        avg_val_loss = sum(val_loss) / len(val_loss)
+        train_losses.append(avg_train_loss)
+        val_losses.append(avg_val_loss)
+        print(
+            f"""Epoch {epoch}:
+            Train: loss = {avg_train_loss}
+            Validation: loss = {avg_val_loss}
+            """
+        )
+
+    plt.plot(numpy.array(train_losses))
+    plt.plot(numpy.array(val_losses))
+    plt.show()
 
 
 if __name__ == '__main__':
-    # Checkpoint is used to track the training model so it could be restored
-    # later to resume training.
-    checkpoint_dir = "checkpoints"
-
-    # Besides checkpoint, `saved_model` is another way to save the model for
-    # inference or optimization.
-    export_dir = "exported"
-
-    # The log directory for tensorboard.
-    log_dir = "logs"
-
-    # The input image's width, height and channels should be consist with your
-    # training data. Here they are set to be complied with the tutorial.
-    input_shape = (128, 128, 3)
-
-    # The number of facial landmarks the model should output. By default the
-    # marks are in 2D space.
-    num_marks = 68
-
-    # Create the Model
-    model = build_landmark_model(input_shape=input_shape,
-                                 output_size=num_marks*2)
-
-    # Prepare for training. First restore the model if any checkpoint file available.
-    if not tf.io.gfile.exists(checkpoint_dir):
-        tf.io.gfile.mkdir(checkpoint_dir)
-        print("Checkpoint directory created: {}".format(checkpoint_dir))
-
-    latest_checkpoint = tf.train.latest_checkpoint(checkpoint_dir)
-    if latest_checkpoint:
-        print("Checkpoint found: {}, restoring...".format(latest_checkpoint))
-        model.load_weights(latest_checkpoint)
-        print("Checkpoint restored: {}".format(latest_checkpoint))
-    else:
-        print("Checkpoint not found. Model weights will be initialized randomly.")
-
-    # Sometimes the user only want to save the model. Skip training in this case.
-    if args.export_only:
-        if not tf.io.gfile.exists(export_dir):
-            tf.io.gfile.mkdir(export_dir)
-
-        if latest_checkpoint is None:
-            print("Warning: Model not restored from any checkpoint.")
-
-        print("Saving model to {} ...".format(export_dir))
-        model.save(export_dir, include_optimizer=False)
-        print("Model saved at: {}".format(export_dir))
-        quit()
-
-    # Finally, it's time to train the model.
-    model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001),
-                  loss=keras.losses.mean_squared_error)
-
-    # Construct a dataset for evaluation.
-    dataset_val = get_parsed_dataset(record_file=args.val_record,
-                                     batch_size=args.batch_size,
-                                     shuffle=False)
-
-    # If evaluation is required only.
-    if args.eval_only:
-        print('Starting to evaluate.')
-        evaluation = model.evaluate(dataset_val)
-        quit()
-
-    # To save and log the training process, we need some callbacks.
-    callback_tb = keras.callbacks.TensorBoard(log_dir=log_dir)
-    callback_checkpoint = keras.callbacks.ModelCheckpoint(
-        filepath=checkpoint_dir+"/landmark",
-        save_weights_only=True,
-        save_best_only=True,
-        monitor='val_loss',
-        verbose=1)
-    callbacks = [callback_tb, callback_checkpoint]
-
-    # Get the training data ready.
-    dataset_train = get_parsed_dataset(record_file=args.train_record,
-                                       batch_size=args.batch_size,
-                                       shuffle=True)
-
-    model.fit(dataset_train, validation_data=dataset_val, epochs=args.epochs,
-              callbacks=callbacks)
+    OUTPUT_SIZE = 1583
+    LEARNING_RATE = 0.001
+    VALIDATION_SIZE = 500
+    args = get_args()
+    main(
+        args.train_record,
+        args.train_index,
+        args.batch_size,
+        LEARNING_RATE,
+        OUTPUT_SIZE,
+        VALIDATION_SIZE
+    )
